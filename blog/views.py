66ea787a9776eb5332472d8724657315c496c47b
João Pedro <jpsconceicao18@gmail.com>
2023-06-21 11:20:30 -0300

    atualiza ola django

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse
 
 # Define uma function view chamada index
 def index(request):
-    return HttpResponse('Olá Django - index')
+    # return HttpResponse('Olá Django - index')
+    return render(request, 'index.html')
 
 # Define uma function view chamada ola.
 def ola(request):
     return HttpResponse('Olá Django')
+    return render(request, 'index.html')
